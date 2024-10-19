62ec908a6c01cdc6e305f083f572750997c0dd34
Aira <villapando.aira@gmail.com>
2024-10-19 21:01:18 +0800

    add model configurations

--- main.py
+++ main.py
@@ -9,9 +9,39 @@ def get_image_summary(path):
     load_dotenv('.secrets')
     api_key = os.getenv('GOOGLE_API_KEY')
 
-    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
     # Configure API client
     genai.configure(api_key=api_key)
+
+    # Configure model
+    model_name = "gemini-1.5-flash"
+    model = genai.GenerativeModel(model_name=model_name)
+
+    generation_config = {
+        "temperature": 0.4,
+        "top_k": 32,
+        "top_p": 1,
+        "max_output_tokens": 4096,
+    }
+
+    # Set safety settings
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        },
+    ]
 
     # Load and encode image data
     try:
@@ -40,7 +70,7 @@ def get_image_summary(path):
 
     # Generate the response using the model
     try:
-        response = model.generate_content(contents=[prompt])
+        response = model.generate_content(contents=[prompt], generation_config=generation_config, safety_settings=safety_settings)
         
         return response._result.candidates[0].content.parts[0].text.strip()
     except Exception as e:
