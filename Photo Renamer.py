03e69b2859fbdb41f151f38b1a0ee31299e971fc
Aira <villapando.aira@gmail.com>
2024-11-20 11:49:23 +0800

    add session finisher

--- Photo Renamer.py
+++ Photo Renamer.py
@@ -2,15 +2,22 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 from genname import process, inits, download
+from streamlit_js_eval import streamlit_js_eval
 
 def process_clicked():
     st.session_state.process_clicked = True
+
+def done_clicked():
+    st.session_state.done_clicked = True
+
 
 inits.init_session_states()
 inits.init_dirs('./')
 
 load_dotenv('vars')
 PAGE_TITLE = os.getenv('PAGE_TITLE')
+DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH')
+SOURCE_PATH = os.getenv('SOURCE_PATH')
 
 
 save_directory = './source'
@@ -35,4 +42,35 @@ if process_button:
     processsed = process.process_summary()
     st.write(processed)
     # if processed:
-    download.download_folder("./output/")
+    download.download_folder(DOWNLOAD_PATH)
+
+if st.session_state.download_clicked:
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.write("## Download")
+        download.download_folder(DOWNLOAD_PATH)
+
+    with col2:
+        st.write("## Finish Session")
+        done_session = st.button("Finish session", on_click=done_clicked, key="Done")
+
+        if done_session or st.session_state.done_clicked:
+            #os.remove(DOWNLOAD_PATH)
+            for i in os.listdir(SOURCE_PATH):
+                os.remove(SOURCE_PATH+'/'+i)
+
+            for i in os.listdir(DOWNLOAD_PATH):
+                os.remove(DOWNLOAD_PATH+'/'+i)
+
+
+            # Clean up the ZIP fi le after download (optional)
+            zip_filename = 'output.zip'
+            if os.path.exists(zip_filename):
+                os.remove(zip_filename)
+
+            
+            streamlit_js_eval(js_expressions="parent.window.location.reload()")
+        
+        
+        
